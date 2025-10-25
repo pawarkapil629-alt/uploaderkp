c3bc8c328dfde202b746143b069ff49c0c2a2a71
pawarkapil629-alt <pawarkapil629@gmail.com>
2025-10-25 12:44:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -777,13 +777,13 @@ async def txt_handler(bot: Client, m: Message):
 
             elif "https://cpvod.testbook.com/" in url or "classplusapp.com/drm/" in url:
                 url = url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/")
-                url = f"https://head-micheline-botupdatevip-f1804c58.koyeb.app/get_keys?url={url}@botupdatevip4u&user_id=604393172"
+                url = f"https://head-micheline-botupdatevip-f1804c58.koyeb.app/get_keys?url=%7Burl%7D@botupdatevip4u&user_id=7703463340"
                 mpd, keys = helper.get_mps_and_keys(url)
                 url = mpd
                 keys_string = " ".join([f"--key {key}" for key in keys])
 
             elif "classplusapp" in url:
-                signed_api = f"https://head-micheline-botupdatevip-f1804c58.koyeb.app/get_keys?url={url}@botupdatevip4u&user_id=604393172"
+                signed_api = f"https://head-micheline-botupdatevip-f1804c58.koyeb.app/get_keys?url=%7Burl%7D@botupdatevip4u&user_id=7703463340"
                 response = requests.get(signed_api, timeout=40)
                 url = response.text.strip()
                 url = response.json()['url']  
